757719b5edf8a3213688a471e62b7da48abf03a6
legendgirlie47 <hongtianyu05@gmail.com>
2026-01-22 15:34:26 +0800

    Set up echo.

--- src/main/java/Ketchup.java
+++ src/main/java/Ketchup.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Ketchup {
     public static void main(String[] args) {
-        String greeting = "Hello I'm Ketchup\nWhat can I do for you?\nBye. Hope to see you again soon! ";
-        System.out.println(greeting);
+        String hello = "Hello! I'm Ketchup\nWhat can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(hello);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(goodbye);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
