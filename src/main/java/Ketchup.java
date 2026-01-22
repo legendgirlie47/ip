3fb07ecbc3cfc63acbf4ef1d53f7595f214d9cfd
legendgirlie47 <hongtianyu05@gmail.com>
2026-01-22 14:34:45 +0800

    Renamed main class, edited to greet and exit.

---
+++ src/main/java/Ketchup.java
@@ -0,0 +1,6 @@
+public class Ketchup {
+    public static void main(String[] args) {
+        String greeting = "Hello I'm Ketchup\nWhat can I do for you?\nBye. Hope to see you again soon! ";
+        System.out.println(greeting);
+    }
+}
